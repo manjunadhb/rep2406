f05bba42e2e04b8c60c4000b2bb18d374c1728ec
manjunadhb <manjunadhb@gmail.com>
2024-10-15 17:19:36 +0530

    added heading by manjunadh

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <h1>This heading is created by Team.</h1>
+      <h1>This heading is created by Manjunadh</h1>
     </div>
   );
 }
